21367ff0980af6a2e2bc60d270a9af0b6cf626c2
ManuelDev <manueldeveloper17@gmail.com>
2022-09-12 15:05:27 -0500

    Add form Nuevo Producto

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between mb-5">
       <div className="container">
         <h1>
           <Link to={"/"} className="text-light">
--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -1,5 +1,48 @@
 const NuevoProducto = () => {
-  return <h1>NuevoProducto</h1>;
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Agregar Nuevo Producto
+            </h2>
+
+            <form>
+              {/* Campo nombre producto */}
+              <div className="form-group">
+                <label>Nombre Producto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Producto"
+                  name="nombre"
+                />
+              </div>
+
+              {/* Campo precio producto */}
+              <div className="form-group">
+                <label>Precio Producto</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Precio Producto"
+                  name="precio"
+                />
+              </div>
+
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export { NuevoProducto };
